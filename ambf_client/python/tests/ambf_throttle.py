b108cf0e75067c2daa0dc8c4776fc7c916a77d34
Adnan Munawar <adnan.munawar@live.com>
2022-03-21 00:35:33 -0400

    Updated from ambf-2.0

--- ambf_client/python/tests/ambf_throttle.py
+++ ambf_client/python/tests/ambf_throttle.py
@@ -3,7 +3,7 @@
 # /*
 #     Software License Agreement (BSD License)
 #     Copyright (c) 2019, AMBF
-#     (www.aimlab.wpi.edu)
+#     (http://practicepoint.wpi.edu)
 
 #     All rights reserved.
 
@@ -36,7 +36,7 @@
 #     ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 #     POSSIBILITY OF SUCH DAMAGE.
 
-#     \author    <http://www.aimlab.wpi.edu>
+#     \author    <http://practicepoint.wpi.edu>
 #     \author    <amunawar@wpi.edu>
 #     \author    Adnan Munawar
 #     \version   0.1
@@ -53,7 +53,7 @@ cmd.step_clock = True
 cmd.n_skip_steps = 1
 
 if len(sys.argv) > 1:
-    print 'Clock Rate Specified as {}'.format(sys.argv[1])
+    print('Clock Rate Specified as {}'.format(sys.argv[1]))
     clock_rate = float(sys.argv[1])
 else:
     clock_rate = 50
